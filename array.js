eb48452aff144a36ab2563233217d496470d2b57
Meher09 <mehernigarcu@gmail.com>
2022-08-03 22:59:02 +0600

    added files

--- array.js
+++ array.js
@@ -1,4 +1,24 @@
+// Practice problem 2
 // You are given an array: var fruits = ['Apple', 'Banana' 'Orange']
 // Find the index of 'banana' and replace 'Banana' with 'Mango'
 // Remove 'Orange' and add 'Watermelon'
 
+var fruits = ["Apple", "Banana", "Orange"];
+var index = fruits.indexOf("Banana");
+console.log(index);
+fruits[1] = "Mango";
+console.log(fruits);
+fruits.pop()
+console.log(fruits)
+fruits.push("Watermelon")
+console.log(fruits)
+
+
+
+
+// You are given three numbers 13, 79 and 45. Write a program that will print the largest number using if-else.
+
+
+
+
+// You are given a triangle with the sides 9,8,9, Write a program to check whether a traingle is isoceles or not using if-elses. [Isoceles => Two sides are equal]
---
+++ condition.js
@@ -0,0 +1,54 @@
+// Practice problem 2
+// You and your friend Tom, Jane, Peter and John got their final exam results. Your total score is 85. Tom's total score is 66, Jane's total score is 95, Peter's total score is 56 and John's total score is 40. The grading chart is 80 or above A grade, 60 or above B grade, 50 or above C grade, 40 or above D grade, 39 or less=> F grade.
+// Write a program to find your and your friends' grades using if-else.
+
+var myNumber = 85;
+var tomsNumber = 66;
+var janesNumber = 95;
+var petersNumber = 56;
+var johnsNumber = 40;
+if (myNumber >= 80){
+    console.log("A grade")
+}
+else if(myNumber>= 70){
+    console.log("B grade")
+}
+else if(myNumber >= 60){
+    console.log("C grade")
+}
+else if (myNumber >= 50){
+    console.log("D grade")
+}
+else {console.log("F grade")
+}
+
+
+if (tomsNumber >= 80){
+    console.log("A grade")
+}
+else if(tomsNumber>= 70){
+    console.log("B grade")
+}
+else if(tomsNumber >= 60){
+    console.log("C grade")
+}
+else if (tomsNumber >= 50){
+    console.log("D grade")
+}
+else {console.log("F grade")
+}
+
+if (janesNumber >= 80){
+    console.log("A grade")
+}
+else if(janesNumber>= 70){
+    console.log("B grade")
+}
+else if(janesNumber >= 60){
+    console.log("C grade")
+}
+else if (janesNumber >= 50){
+    console.log("D grade")
+}
+else {console.log("F grade")
+}
